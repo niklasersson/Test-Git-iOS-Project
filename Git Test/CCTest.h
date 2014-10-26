f3448e5a303fefd02f8b0b7cb5d6fc9ac2b038a2
Niklas Ersson <niklasersson@Niklass-MacBook-Air.local>
2014-10-26 19:21:10 +0100

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Niklas Ersson on 2014-10-26.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Niklas Ersson on 2014-10-26.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
